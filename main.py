4fd72f08972c8b7d7e67923ed7f15aaf37d81169
Vladimir Zapata <vzapata311@gmail.com>
2024-03-31 00:09:22 -0400

    .

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
 import json
 
 dgm_df = pd.read_csv('Nominas/nomina_dgm.csv')
@@ -17,7 +24,6 @@ dgm_df.rename(columns=rename_dict, inplace=True)
 dgm_df.rename(columns={'SUELDO_BRUTO_(RD$)': 'SUELDO_BRUTO'}, inplace=True)
 
 dgm_df['GENERO'] = dgm_df['GENERO'].str.upper()
-dgm_df['INSTITUCION'] = 'DIRECCION GENERAL DE MIGRACION'
 
 dgm_df['SUELDO_BRUTO'] = dgm_df['SUELDO_BRUTO'].str.replace(',', '')
 dgm_df['ISR'] = dgm_df['ISR'].str.replace(',', '')
@@ -30,56 +36,57 @@ dgm_df['AFP'] = pd.to_numeric(dgm_df['AFP'], errors='coerce')
 dgm_df['SFS'] = pd.to_numeric(dgm_df['SFS'], errors='coerce')
 
 dgm_df['SUELDO_NETO'] = dgm_df['SUELDO_BRUTO'] - dgm_df['AFP'] - dgm_df['SFS'] - dgm_df['ISR']
+dgm_df['INSTITUCION'] = 'DIRECCION GENERAL DE MIGRACION'
 
 non_essential_columns = ['NOMBRE', 'ESTATUS', 'TOTAL_DESC.', 'NETO', 'OTROS_DESC.']
 dgm_df = dgm_df.drop(columns=non_essential_columns)
 
 dgm_df.fillna(0, inplace=True)
 
-numeric_columns = dgm_df.select_dtypes(include=['float64', 'int64']).columns
-
-# Increase the figure size here by increasing the width and height
-fig, axs = plt.subplots(len(numeric_columns), 3, figsize=(20, 8 * len(numeric_columns)))
-
-for i, column in enumerate(numeric_columns):
-    # Histogram
-    axs[i, 0].hist(dgm_df[column], bins=20, color='skyblue', edgecolor='black')
-    axs[i, 0].set_xlabel(column)
-    axs[i, 0].set_ylabel('FRECUENCIA')
-    axs[i, 0].set_title(f'HISTOGRAMA DE {column}')
-
-    # KDE plot
-    dgm_df[column].plot.kde(ax=axs[i, 1], color='skyblue')
-    axs[i, 1].set_xlabel(column)
-    axs[i, 1].set_ylabel('DENSIDAD')
-    axs[i, 1].set_title(f'ESTIMACION DE DENSIDAD DEL NUCLEO DE {column}')
-
-    # Box plot
-    axs[i, 2].boxplot(dgm_df[column])
-    axs[i, 2].set_xlabel(column)
-    axs[i, 2].set_title(f'DIAGRAMA DE CAJA DE {column}')
-
-    # # Print statistics
-    # statistics = dgm_df[column].describe()
-    # print(f"\nEstadísticas de {column}:")
-    # print(statistics)
-
-plt.subplots_adjust(hspace=0.5, wspace=0.3)
-plt.show()
+# numeric_columns = dgm_df.select_dtypes(include=['float64', 'int64']).columns
+#
+# # Increase the figure size here by increasing the width and height
+# fig, axs = plt.subplots(len(numeric_columns), 3, figsize=(20, 8 * len(numeric_columns)))
+#
+# for i, column in enumerate(numeric_columns):
+#     # Histogram
+#     axs[i, 0].hist(dgm_df[column], bins=20, color='skyblue', edgecolor='black')
+#     axs[i, 0].set_xlabel(column)
+#     axs[i, 0].set_ylabel('FRECUENCIA')
+#     axs[i, 0].set_title(f'HISTOGRAMA DE {column}')
+#
+#     # KDE plot
+#     dgm_df[column].plot.kde(ax=axs[i, 1], color='skyblue')
+#     axs[i, 1].set_xlabel(column)
+#     axs[i, 1].set_ylabel('DENSIDAD')
+#     axs[i, 1].set_title(f'ESTIMACION DE DENSIDAD DEL NUCLEO DE {column}')
+#
+#     # Box plot
+#     axs[i, 2].boxplot(dgm_df[column])
+#     axs[i, 2].set_xlabel(column)
+#     axs[i, 2].set_title(f'DIAGRAMA DE CAJA DE {column}')
+#
+#     # # Print statistics
+#     # statistics = dgm_df[column].describe()
+#     # print(f"\nEstadísticas de {column}:")
+#     # print(statistics)
+#
+# plt.subplots_adjust(hspace=0.5, wspace=0.3)
+# plt.show()
 
 ohe = OneHotEncoder()
 sts = StandardScaler()
 
-encoder_dict = {}
-for column in dgm_df.select_dtypes(include=['object']).columns:
-    encoded_data = ohe.fit_transform(dgm_df[[column]]).toarray()
-    encoded_df = pd.DataFrame(encoded_data, columns=[f'{column}_{cat}' for cat in ohe.categories_[0]],
-                              index=dgm_df.index)
-    dgm_df = pd.concat([dgm_df, encoded_df], axis=1)
-    encoder_dict[column] = {str(cat): i for i, cat in enumerate(ohe.categories_[0])}
-
-with open('encoder_dict.json', 'w') as f:
-    json.dump(encoder_dict, f)
+# encoder_dict = {}
+# for column in dgm_df.select_dtypes(include=['object']).columns:
+#     encoded_data = ohe.fit_transform(dgm_df[[column]]).toarray()
+#     encoded_df = pd.DataFrame(encoded_data, columns=[f'{column}_{cat}' for cat in ohe.categories_[0]],
+#                               index=dgm_df.index)
+#     dgm_df = pd.concat([dgm_df, encoded_df], axis=1)
+#     encoder_dict[column] = {str(cat): i for i, cat in enumerate(ohe.categories_[0])}
+#
+# with open('encoder_dict.json', 'w') as f:
+#     json.dump(encoder_dict, f)
 
 features = [
     "GENERO",
@@ -99,8 +106,41 @@ num_feats = sts.fit_transform(x.select_dtypes("number"))
 
 xp = np.hstack([cat_feats, num_feats])
 
-# print(dgm_df)
-# print(scaled_dgm_df)
+models = [
+    ("RF", RandomForestRegressor()),
+    ("KNN", KNeighborsRegressor()),
+    ("LR", LinearRegression())
+]
+
+x_train, x_test, y_train, y_test = train_test_split(xp, y, test_size=0.2)
+
+model_performance = {}
+
+for name, model in models:
+    print(name)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    mae = mean_absolute_error(y_pred, y_test)
+    mse = mean_squared_error(y_pred, y_test)
+    model_performance[name] = {'MAE': mae, 'MSE': mse}
+    print("MAE:", mae)
+    print("MSE:", mse)
+    print("")
+
+best_model_name = min(model_performance, key=lambda k: model_performance[k]['MAE'])
+best_model = [model for name, model in models if name == best_model_name][0]
+print(f"Best Model: {best_model_name}")
+
+y_pred_best = best_model.predict(x_test)
+
+results_df = pd.concat([
+    x.loc[y_test.index].reset_index(drop=True),  # Test features
+    y_test.reset_index(drop=True),               # True labels
+    pd.Series(y_pred_best, name="Predicted")     # Predictions from best model
+], axis=1)
+
+# print(results_df)
+
 
 # print(xp)
 
